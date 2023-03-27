7eb82834893e77dd375ce0f930902424a955ddc9
Shinichi <miyakawashi@gmail.com>
2023-03-26 23:49:33 -0400

    add hover attribute, 0th note, tab @ bottom

--- main/src/components/String.tsx
+++ main/src/components/String.tsx
@@ -5,32 +5,50 @@ interface StringI {
 export const String: React.FC<StringI> = ({ stringLetter }): JSX.Element => {
   const numFrets = 16;
   const handleNoteClick = (e: React.MouseEvent) => {
-    console.log(stringLetter, " - ", e.currentTarget.innerHTML);
+    console.log(stringLetter, " - ", e.currentTarget.id);
+    // TODO
+    // Save User input for notes in database?
   };
   const populateStringFrets = (numFrets: number) => {
     const holdNotes = [];
-    for (let index = 0; index < numFrets; index++) {
+    for (let index = 0; index <= numFrets; index++) {
       holdNotes.push(
         <span
           className="note-container"
-          key={`${stringLetter}-${index + 1}-container`}
+          key={`${stringLetter}-${index}-container`}
         >
           <hr />
+
           <div
             className="note"
-            key={`${stringLetter}-${index + 1}`}
+            key={`${stringLetter}-${index}`}
             onClick={(e) => handleNoteClick(e)}
+            id={`${index + 1}`}
           >
-            {index + 1}
+            <div className="circle">{index}</div>
           </div>
         </span>
       );
     }
     return holdNotes;
   };
-
-  return (
-    <span className="string-note-container">
+  return stringLetter === "G" ? (
+    <span
+      className="string-note-container"
+      style={{
+        borderTop: "solid black 0.5vh",
+        borderBottom: "solid black 0.5vh",
+      }}
+    >
+      {populateStringFrets(numFrets)}
+    </span>
+  ) : (
+    <span
+      className="string-note-container"
+      style={{
+        borderBottom: "solid black 0.5vh",
+      }}
+    >
       {populateStringFrets(numFrets)}
     </span>
   );
--- main/src/components/Tab.tsx
+++ main/src/components/Tab.tsx
@@ -1,9 +1,59 @@
 export const Tab = (): JSX.Element => {
+  // TODO
+  // Change type of tabSections when we figure out what kind of data we want to store
+  const createMeasure= () => {
+    const tabSections: any = [];
+    for (let i = 0; i < 16; i++) {
+      if (i % 4 === 0 && i !== 0) {
+        tabSections.push(
+          <div
+            style={{
+              height: "15vh",
+              border: "1px solid white",
+              marginLeft: "1vw",
+            }}
+          ></div>
+        );
+      }
+      tabSections.push(
+        <ul style={{ height: "15vh", paddingLeft: "1vw" }}>
+          <li>-</li>
+          <li>-</li>
+          <li>-</li>
+          <li>-</li>
+        </ul>
+      );
+    }
+    return tabSections;
+  };
   return (
-    <div>
-      <ul>
-        <li>Testing Tab Component</li>
+    <div
+      style={{
+        display: "flex",
+        marginTop: "5vh"
+      }}
+    >
+      <ul
+        style={{
+          paddingLeft: "1vw",
+          width: "fit-content",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          color: "whitesmoke",
+          height: "15vh",
+        }}
+      >
+        <li>G</li>
+        <li>D</li>
+        <li>A</li>
+        <li>E</li>
       </ul>
+
+      <div style={{ height: "15vh", border: "1px solid white" }}></div>
+      <div style={{ width: "100vw", display: "flex" }}>
+        {createMeasure()}
+      </div>
     </div>
   );
 };
